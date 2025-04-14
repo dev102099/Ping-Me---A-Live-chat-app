9846ff27d1cb67c3298d962d2ff0b09e306aaf14
Dev Parpyani <sonofhades10@Devs-MacBook-Air.local>
2025-04-14 15:43:29 +0530

    issues

--- api/middleware/verifyCookie.js
+++ api/middleware/verifyCookie.js
@@ -4,10 +4,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 const verifyCookie = async (req, res, next) => {
   const token = req.cookies.access_token;
+  const JWT_TOKEN= process.env.JWT_SECRET;
+  if (!JWT_TOKEN) {
+    res.status(500).json({ message: "Internal Server Error." });
+  }
   if (!token) {
     res.status(401).json({ message: "Unauthorized." });
   }
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, , (err, user) => {
     if (err) {
       next(err);
     }

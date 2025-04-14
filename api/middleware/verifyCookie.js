ab7e12fde48ac0c27ebb5d5da1a938cccecc379e
Dev Parpyani <sonofhades10@Devs-MacBook-Air.local>
2025-04-14 15:54:40 +0530

    debugging

--- api/middleware/verifyCookie.js
+++ api/middleware/verifyCookie.js
@@ -8,6 +8,7 @@ const verifyCookie = async (req, res, next) => {
   if (!JWT_TOKEN) {
     res.status(500).json({ message: "Internal Server Error." });
   }
+  console.log(token);
   if (!token) {
     res.status(401).json({ message: "Unauthorized." });
   }

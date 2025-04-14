600ffbdc579c76c7c3bb7064c29c4540f72fc0ad
Dev Parpyani <sonofhades10@Devs-MacBook-Air.local>
2025-04-14 15:21:08 +0530

    bugs and issues

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
-
-const socket = io("http://localhost:3000", {
+const SERVER = import.meta.env.VITE_SOCKET_URL;
+const socket = io(`${SERVER}`, {
   withCredentials: true,
   transports: ["websocket"],
   autoConnect: false,

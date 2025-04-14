25e2f996b3fd53ac068fbde63a70c30e783c869e
Dev Parpyani <sonofhades10@Devs-MacBook-Air.local>
2025-04-14 16:44:34 +0530

    issues

--- api/Controllers/userController.js
+++ api/Controllers/userController.js
@@ -25,7 +25,7 @@ const handleLog = async (req, res, next) => {
       .status(200)
       .cookie("access_token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: true,
         sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })

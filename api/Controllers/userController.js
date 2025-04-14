3286ffdc63233b14d8904ad7723038228191a604
Dev Parpyani <sonofhades10@Devs-MacBook-Air.local>
2025-04-14 16:40:01 +0530

    issues

--- api/Controllers/userController.js
+++ api/Controllers/userController.js
@@ -27,6 +27,7 @@ const handleLog = async (req, res, next) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "none",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
       })
       .json({ restData });
   } catch (error) {
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), tailwindcss()],
+  server: {
+    historyApiFallback: true, // Critical for client-side routing
+  },
+  build: {
+    outDir: "dist", // Ensure this matches Render's publish dir
+  },
 });
